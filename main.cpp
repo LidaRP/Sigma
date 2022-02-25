f71ff903637741c986fc29d017e8149ff93b5414
LidaRP <rahmanipiyani.lida@gmail.com>
2022-02-25 16:15:47 +0330

    sigma

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+           {
+            int x,n=0;
+           	cout<<"Please enter a number:\n";
+           	cin>>x;
+           	    for(int i=1;i<=x;i++)
+           	        {
+           	        	n+=i;
+					}
+				cout<<n;	
+return 0;
+           }
